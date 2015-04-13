8cd97b3a5eed9a033f89e6999b2fa3ed796f461e
mohan-zsl <mohanr@zenithsoft.com>
2015-04-13 15:23:19 +0530

    en_AU - TheClub.html is completed

---
+++ js/data_lang.js
@@ -0,0 +1,134 @@
+var langDataObj =
+	{
+	"en_AU":
+		{
+			"en-headLine": "More extraordinary with every stay",
+			"en-head-sub-nav1": "The Club",
+			"en-head-sub-nav2": "Benefits",
+			"en-head-sub-nav3": "Club Hotels",
+			"en-head-sub-nav4": "Book",
+			"en-reg-headLine1": "Become a member",
+			"en-reg-subHead": "Sign up today and start<br /> enjoying the benefits of<br /> being a HotelClub Member.",
+			"en-reg-cta": "Join now >",
+			"en-reg-logged-headLine": "Where’s your next holiday?",
+			"en-reg-logged-subHead": "Start searching for the perfect place to stay from any of our hotels in over 7,400 cities worldwide.",
+			"en-reg-logged-CTA": "Get booking >",
+			"en-club-sub-head1": "Welcome to HotelClub",
+			"en-club-sub-head2": "We want our travellers to have the best travel experience out there.",
+			"en-club-sub-head3": "So from the moment you book, your whole journey will be rich with rewards, flexibility, better benefits and greater experiences. And when you return home, it starts over, because every time you book with us, you'll get even more.",
+			"en-club-sub-head4": "Become a HotelClub Member and experience an extraordinary way to stay.",
+			"en-club-sub-headLine": "What you'll experience",
+			"en-club-sub-subHead1": "There's something to look forward to at every stage of your journey.",
+			"en-club-sub-subHead2": "Before you leave >",
+			"en-club-sub-subHead3": "Get reserved member pricing, our 'best price assurance' and booking support from our travel specialists. Plus, our members earn up to 7%  back on every booking.",
+			"en-club-sub-subHead4": "During your stay >", 
+			"en-club-sub-subHead5": "Enjoy member benefits including instant upgrades, club lounge access, free breakfast, Wi-Fi and late check-outs at over 5,000 hotels.",
+			"en-club-cta1":"Find out more >",
+			"en-club-cta1-headLine1": "How our points work",
+			"en-club-cta1-headLine2": "Earn and redeem immediately after your first stay.",
+			"en-club-cta1-headLine3": "Great earning potential",
+			"en-club-cta1-headLine4": "Receive points called Member Rewards every time you book your stay with us. All our hotels are eligible so the more you stay, the better the rewards.",
+			"en-club-cta1-headLine5": "1 Member Reward = US$1",
+			"en-club-cta1-headLine6": "Earn up to 7% of the total amount of your booking. Your points can be used as soon as you've completed your stay.",
+			"en-club-cta1-headLine7": "Spend when it suits you",
+			"en-club-cta1-headLine8": "With no waiting and blackout periods, you can spend your Member Rewards whenever you like, at any hotel in the world.",
+			"en-club-cta2": "Find out more >",
+			"en-club-cta2-headLine1":"Earn more with our app",
+			"en-club-cta2-headLine2":"Enjoy more convenience, receive mobile-only offers wherever you are in the world and earn more Member Rewards whenever you book on our app.",
+			"en-benefit-headLine":"Your member benefits in more detail",
+			"en-benefit-sub-headLine":"Our members enjoy more than just points. We seek out the best partners from across the globe to turn every stay into an extraordinary experience.",
+			"en-benefit-head1-headLine1":"Before you leave",
+			"en-benefit-head1-headLine2":"Exclusive offers",
+			"en-benefit-head1-headLine3":"Be the first to discover our latest sales, member offers and reserved member pricing.",
+			"en-benefit-head1-headLine4":"Personalised recommendations",
+			"en-benefit-head1-headLine5":"Enjoy exclusive hotel recommendations tailored especially for your travel tastes.",
+			"en-benefit-head1-headLine6":"Travel support",
+			"en-benefit-head1-headLine7":"We’re here to help make planning easier and your trip go more smoothly.",
+			"en-benefit-head1-headLine8":"Best price assurance",
+			"en-benefit-head1-headLine9":"If you happen to find a better price anywhere else online, we'll match it.",
+			"en-benefit-head1-headLine10":"Member Rewards",
+			"en-benefit-head1-headLine11":"Your points are ready to use on your very next stay. Spend them when you like, at any hotel of your choice.",
+			"en-benefit-head2-Line1":"During your stay",
+			"en-benefit-head2-Line2":"Room upgrades",
+			"en-benefit-head2-Line3":"Enjoy an instant room upgrade on arrival when you book one of our Club Hotels.",
+			"en-benefit-head2-Line4":"Club lounge access",
+			"en-benefit-head2-Line5":"Relax in the stylish, executive areas of your hotel with refreshments to enjoy throughout the day.",
+			"en-benefit-head2-Line6":"Late check-outs and priority check-ins*",
+			"en-benefit-head2-Line7":"To make your trip go even more smoothly, our members receive priority check-ins and late check-outs. Perfect for lazy mornings.",
+			"en-benefit-head2-Line8":"Free breakfast and dining offers",
+			"en-benefit-head2-Line9":"While you're away, your taste buds will be taken care of with complimentary breakfasts as well as delicious meals for less.",
+			"en-benefit-head2-Line10":"Free Wi-Fi",
+			"en-benefit-head2-Line11":"Stay connected with complimentary Wi-Fi throughout your stay.",
+			"en-clubHotel-head1":"Welcome to your instant upgrade",
+			"en-clubHotel-head2":"We partner with selected hotels to make your next stay even more luxurious. Get an instant upgrade plus extra special benefits like complimentary breakfast, Wi-Fi, late check outs and waived resort fees when you book one of our Club Hotels. Available for our Gold and Platinum Members only.",
+			"en-clubHotel-head3":"Join HotelClub today and start experiencing these rewards and more with every stay.",
+		},		
+	"zh_TW":
+		{
+			title:"zh_TW"
+		},
+	"zh_HK":
+		{
+			title:"zh_HK"
+		},	
+	"zh_CN":
+		{
+			title:"zh_CN"
+		},
+	"ja_JP":
+		{
+			title:"ja_JP"
+		},
+	"ko_KR":
+		{
+			title:"ko_KR"
+		},
+	"th_TH":
+		{
+			title:"th_TH"
+		},
+	"ms_MY":
+		{
+			title:"ms_MY"
+		},
+	"id_ID":
+		{
+			title:"id_ID"
+		},
+	"nl_NL":
+		{
+			title:"nl_NL"
+		},
+	"de_DE":
+		{
+			title:"de_DE"
+		},
+	"sv_SE":
+		{
+			title:"sv_SE"
+		},
+	"es_ES":
+		{
+			title:"es_ES"
+		},
+	"fr_FR":
+		{
+			title:"fr_FR"
+		},
+	"it_IT":
+		{
+			title:"it_IT"
+		},
+	"pt_PT":
+		{
+			title:"pt_PT"
+		},
+	"pl_PL":
+		{
+			title:"pl_PL"
+		},
+	"ru_RU":
+		{
+			title:"ru_RU"
+		}
+	};
